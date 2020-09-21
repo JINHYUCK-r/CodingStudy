24cea20659074586116cad772e29d1cc0c8dd9d9
JINHYUCK-r <wlsgur7701@naver.com>
2020-09-21 17:46:43 +0900

    BufferedReader사용

---
+++ src/com/CodigStudy/Main.java
@@ -0,0 +1,25 @@
+package com.CodigStudy;
+import java.io.*;
+
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine().trim());
+        
+        for (int i=0; i < n; i++) {
+            String text = br.readLine();
+            String[] word = text.split(" ");
+            int a = Integer.parseInt(word[0]);
+            int b = Integer.parseInt(word[1]);
+            bw.write((a+b) + "\n");
+        }
+        
+        bw.flush();
+        bw.close();
+    }
+
+
+}
